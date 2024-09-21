4a9da09e37856af17944d00fe70d43967969999d
Paul Obinna <paulobinna493@gmail.com>
2024-09-21 12:06:57 +0100

    added the xicon

--- js/app.js
+++ js/app.js
@@ -1,13 +1,28 @@
-const hamburger = document.querySelector(".icon");
+const hamburger = document.querySelector(".hamburger");
+const xIcon = document.querySelector(".x-icon");
+const menuIcon = document.querySelector(".icon");
 const menu = document.querySelector(".navLinks");
+
 //Event
 hamburger.addEventListener("click", handleOpen);
 
 //Fuction
 function handleOpen() {
-  // menu.style.display = "block";
+ 
+  if (xIcon.style.display === "block") {
+      // If the xIcon is visible, hide it and show the menu icon
+    xIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    // If the xIcon is hidden, show it and hide the menu icon
+    xIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
   menu.classList.toggle("toggle");
 }
+ // xIcon.style.display = "block";
+  // menuIcon.style.display = "none";
+  // menu.classList.toggle("toggle");
 
 // hamburger.onclick = function () {
 //   console.log("clicked");
